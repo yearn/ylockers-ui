11f047d3df2c6a9c2f445584a58c2b45bf94c3ed
Marco Guaspari Worms <marcogworms@gmail.com>
2024-03-18 23:37:18 -0300

    add landing page styles

--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,11 @@ const Button = ({ type="default", disabled=false, children }) => (
 );
 
 const Header = () => (
-  <header className="flex justify-between items-center w-full h-[72px]">
+  <header className="flex justify-between items-center w-full h-[72px] z-10 my-2">
     <div className="space-x-4">
-      <a href="#" className="p-4">Earn</a>
-      <a href="#" className="p-4">About</a>
-      <a href="#" className="p-4">Expired farms</a>
+      <a href="#" className="mx-2 py-2 font-bold border-b-2">Earn</a>
+      <a href="#" className="mx-2 py-2 font-light">About</a>
+      <a href="#" className="mx-2 py-2 font-light">Expired farms</a>
     </div>
     <Button type="transparent">Launch App</Button>
   </header>
@@ -25,14 +25,15 @@ const Header = () => (
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-start min-h-screen px-48 bg-gradient-to-r from-dark-black to-dark-blue">
+    <main className="flex flex-col items-start min-h-screen px-80 bg-gradient-to-r from-dark-black to-dark-blue text-white">
+      <Image className="absolute top-o left-[24vw] w-[76vw]" src="/prisma.svg" width={200} height={200} />
       <Header />
-      <section className="mt-[30vh]">
+      <section className="mt-[27vh]">
         <h1 className="text-6xl font-bold">Put your<br />yPRISMA to work</h1>
-        <p className="my-8">Each week, Yearn’s vePRISMA position earns protocol revenue, bribes, and boosting fees.<br />This revenue is converted to mkUSD stablecoin and distributed to yPRISMA stakers at the start of the week.</p>
+        <p className="my-8 w-[670px] font-thin">Each week, Yearn’s vePRISMA position earns protocol revenue, bribes, and boosting fees.<br />This revenue is converted to mkUSD stablecoin and distributed to yPRISMA stakers at the start of the week.</p>
         <div className="flex items-center space-x-4">
           <Button>Launch App</Button>
-          <h2 className="text-3xl font-bold text-light-blue">APR 137.91%</h2>
+          <h2 className="text-3xl font-bold text-light-blue font-mono">APR 137.91%</h2>
         </div>
       </section>
     </main>
