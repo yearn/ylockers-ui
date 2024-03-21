8871d410e4528d6e803bdfed9568fa7e5273548a
Marco Guaspari Worms <marcogworms@gmail.com>
2024-03-20 23:52:41 -0300

    move header to own component

---
+++ app/components/controls/Header.tsx
@@ -0,0 +1,14 @@
+import Button from "./Button";
+
+const Header = () => (
+  <header className="flex justify-between items-center w-full h-[72px] z-10 my-2">
+    <div className="space-x-4">
+      <a href="#" className="mx-2 py-2 font-bold border-b-2">Earn</a>
+      <a href="#" className="mx-2 py-2 font-light">About</a>
+      <a href="#" className="mx-2 py-2 font-light">Expired farms</a>
+    </div>
+    <Button style="transparent">Launch App</Button>
+  </header>
+)
+
+export default Header
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,6 @@
 import Image from "next/image";
 import Button from "./components/controls/Button";
-
-const Header = () => (
-  <header className="flex justify-between items-center w-full h-[72px] z-10 my-2">
-    <div className="space-x-4">
-      <a href="#" className="mx-2 py-2 font-bold border-b-2">Earn</a>
-      <a href="#" className="mx-2 py-2 font-light">About</a>
-      <a href="#" className="mx-2 py-2 font-light">Expired farms</a>
-    </div>
-    <Button style="transparent">Launch App</Button>
-  </header>
-)
+import Header from "./components/controls/Header";
 
 export default function Home() {
   return (
