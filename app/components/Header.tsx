5367fc5e06567423c4816464384e2c147424f4ca
Marco Guaspari Worms <marcogworms@gmail.com>
2024-03-22 17:30:14 -0300

    fix header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = ({ items, selected="Earn", launchApp=true }) => (
   <header className={`flex justify-between items-center w-[1200px] h-[72px] z-10 my-2 ${launchApp === false ? '' : ''}`}>
     <div className="space-x-4">
       {items.map((item) => (
-        <Link href={item.link} key={item.text} className={`mx-2 py-2 font-light hover:border-b-2 ${selected === item.text ? 'border-b-2 font-bold' : ''}`}>
+        <Link href={item.link} key={item.text} className={`mx-2 py-2 hover:border-b-2 ${selected === item.text ? 'border-b-2 font-bold' : 'font-light'}`}>
           {item.text}
         </Link>
       ))}
