7405165386264d029bfb31dbd93076f447a1661c
murderteeth <murdertxxth@gmail.com>
2024-04-12 04:08:11 +0000

    tone it down

--- app/components/Tokens.tsx
+++ app/components/Tokens.tsx
@@ -1,19 +1,21 @@
 import { fNumber } from '@/lib/format'
-import React, { forwardRef, HtmlHTMLAttributes } from 'react'
 import { formatUnits } from 'viem'
 
-type TokensProps = HtmlHTMLAttributes<HTMLDivElement> & {
+export default function Tokens({
+  amount,
+  decimals,
+  humanize,
+  accuracy,
+  padStart,
+  className
+}: {
   amount: bigint
   decimals: number
   humanize?: boolean
   accuracy?: number 
   padStart?: number
-  className?: string
-}
-
-const Tokens = forwardRef<HTMLSpanElement, TokensProps>(
-  ({ amount, decimals, humanize, accuracy, padStart, className, ...props }, ref
-) => {  
+  className?: string 
+}) {
   const formatted = (() => {
     const units = formatUnits(amount, decimals)
     if (humanize) return fNumber(Number(units), { fixed: 0 })
@@ -22,11 +24,7 @@ const Tokens = forwardRef<HTMLSpanElement, TokensProps>(
     return `${whole.padStart(padStart || 0, '0')}.${(fraction || '0'.repeat(accuracy || 2)).slice(0, accuracy || 2)}`
   })()
 
-  return <span ref={ref} {...props} className={`font-mono ${className}`}>
+  return <span className={`font-mono ${className}`}>
     {formatted}
   </span>
-})
-
-Tokens.displayName = 'Tokens'
-
-export default Tokens
+}
