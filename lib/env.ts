7260446a80cff520a8ff1a82e385250bbbf71193
Marco Worms <marcogworms@gmail.com>
2024-04-17 01:29:11 -0300

    revert env config

--- lib/env.ts
+++ lib/env.ts
@@ -6,9 +6,9 @@ function address(envar: string | undefined) {
 
 const env = {
   address,
-  PRISMA: address('0xdA47862a83dac0c112BA89c6abC2159b95afd71C'),
-  YPRISMA: address('0xe3668873D944E4A949DA05fc8bDE419eFF543882'),
-  YPRISMA_BOOSTED_STAKER: address('0x4c04377f90Eb1E42D845AB21De874803B8773669'),
+  PRISMA: address(process.env.NEXT_PUBLIC_PRISMA),
+  YPRISMA: address(process.env.NEXT_PUBLIC_YPRISMA),
+  YPRISMA_BOOSTED_STAKER: address(process.env.NEXT_PUBLIC_YPRISMA_BOOSTED_STAKER),
   DEV: !process.env.NODE_ENV || process.env.NODE_ENV === 'development',
 }
 
