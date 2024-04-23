6fa324f47bf0ff123c6285c662c160651369ec17
murderteeth <murdertxxth@gmail.com>
2024-04-23 05:07:57 +0000

    Nerf envar errors

--- lib/types.ts
+++ lib/types.ts
@@ -1,4 +1,4 @@
 import { z } from 'zod'
 
-export const zhexstringSchema = z.custom<`0x${string}`>((val: any) => /^0x/.test(val))
+export const zhexstringSchema = z.custom<`0x${string}`>((val: any) => /^0x/.test(val), 'Required')
 export type zhexstring = z.infer<typeof zhexstringSchema>
