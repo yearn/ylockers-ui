bbd5b3ca2ccb1cd2b0164d6771bb6c467bfd8fca
Marco Worms <marcogworms@gmail.com>
2024-05-02 00:42:23 -0300

    try to fix again

--- lib/apr.ts
+++ lib/apr.ts
@@ -1,27 +1,27 @@
-import { useContractReads } from 'wagmi';
+import { useContractRead } from 'wagmi';
 import { erc20Abi } from 'viem';
 import env from "@/lib/env";
 import abis from "@/app/abis";
 import bmath from "@/lib/bmath";
 
 export function useAverageApr(totalSupply: bigint, yPrismaPrice: number, yvmkUsdPrice: number) {
-  const { data } = useContractReads({
-    contracts: [
-      {
-        address: env.YPRISMA_REWARDS_DISTRIBUTOR,
-        abi: abis.SingleTokenRewardDistributor,
-        functionName: 'getWeek',
-      },
-      {
-        address: env.YPRISMA_REWARDS_DISTRIBUTOR,
-        abi: abis.SingleTokenRewardDistributor,
-        functionName: 'weeklyRewardAmount',
-        args: [data?.[0]?.result ? data[0].result - 1 : 0],
-      },
-    ],
+  const { data: currentWeek } = useContractRead({
+    address: env.YPRISMA_REWARDS_DISTRIBUTOR,
+    abi: abis.SingleTokenRewardDistributor,
+    functionName: 'getWeek',
   });
 
-  const lastWeekAmount = data?.[1]?.result ?? 0n;
+  const lastWeek = currentWeek && currentWeek > 0 ? currentWeek - 1 : 0;
+
+  const { data: lastWeekAmount } = useContractRead({
+    address: env.YPRISMA_REWARDS_DISTRIBUTOR,
+    abi: abis.SingleTokenRewardDistributor,
+    functionName: 'weeklyRewardAmount',
+    args: [lastWeek],
+  });
+
+  if (!lastWeekAmount) return 0;
+
   const averageRewards = bmath.div(lastWeekAmount, totalSupply);
   const averageRewardsUsd = bmath.mul(averageRewards, yvmkUsdPrice);
   const capitalValue = bmath.mul(yPrismaPrice, bmath.div(totalSupply, 1e18));
@@ -30,38 +30,37 @@ export function useAverageApr(totalSupply: bigint, yPrismaPrice: number, yvmkUsd
 }
 
 export function useAccountApr(account: string, balance: bigint, totalSupply: bigint, yPrismaPrice: number, yvmkUsdPrice: number) {
-  const { data } = useContractReads({
-    contracts: [
-      {
-        address: env.YPRISMA_REWARDS_DISTRIBUTOR,
-        abi: abis.SingleTokenRewardDistributor,
-        functionName: 'getWeek',
-      },
-      {
-        address: env.YPRISMA_REWARDS_DISTRIBUTOR,
-        abi: abis.SingleTokenRewardDistributor,
-        functionName: 'weeklyRewardAmount',
-        args: [data?.[0]?.result ? data[0].result - 1 : 0],
-      },
-      {
-        address: env.YPRISMA_BOOSTED_STAKER,
-        abi: abis.YearnBoostedStaker,
-        functionName: 'decimals',
-      },
-      {
-        address: env.YPRISMA_BOOSTED_STAKER,
-        abi: abis.YearnBoostedStaker,
-        functionName: 'getAccountWeightRatioAt',
-        args: [account, data?.[0]?.result ? data[0].result - 1 : 0],
-      },
-    ],
+  const { data: currentWeek } = useContractRead({
+    address: env.YPRISMA_REWARDS_DISTRIBUTOR,
+    abi: abis.SingleTokenRewardDistributor,
+    functionName: 'getWeek',
   });
 
-  const lastWeekAmount = data?.[1]?.result ?? 0n;
-  const ybsDecimals = data?.[2]?.result ?? 0n;
-  const userShare = bmath.div(data?.[3]?.result ?? 0n, 10n ** ybsDecimals);
-  const userRewards = bmath.div(bmath.mul(lastWeekAmount, userShare), 1e18);
+  const lastWeek = currentWeek && currentWeek > 0 ? currentWeek - 1 : 0;
 
+  const { data: lastWeekAmount } = useContractRead({
+    address: env.YPRISMA_REWARDS_DISTRIBUTOR,
+    abi: abis.SingleTokenRewardDistributor,
+    functionName: 'weeklyRewardAmount',
+    args: [lastWeek],
+  });
+
+  const { data: ybsDecimals } = useContractRead({
+    address: env.YPRISMA_BOOSTED_STAKER,
+    abi: abis.YearnBoostedStaker,
+    functionName: 'decimals',
+  });
+
+  const { data: userShare } = useContractRead({
+    address: env.YPRISMA_BOOSTED_STAKER,
+    abi: abis.YearnBoostedStaker,
+    functionName: 'getAccountWeightRatioAt',
+    args: [account, lastWeek],
+  });
+
+  if (!lastWeekAmount || !ybsDecimals || !userShare) return 0;
+
+  const userRewards = bmath.div(bmath.mul(lastWeekAmount, bmath.div(userShare, 10n ** ybsDecimals)), 1e18);
   const userRewardsUsd = bmath.mul(userRewards, yvmkUsdPrice);
   const capitalValue = bmath.mul(yPrismaPrice, bmath.div(balance, 1e18));
 
