91b7a1cb90c7d513597c429620263c0d6e0d9445
Marco Worms <marcogworms@gmail.com>
2024-05-07 12:21:35 -0300

    fix #53

--- app/components/WithdrawAllFromOldStaker.tsx
+++ app/components/WithdrawAllFromOldStaker.tsx
@@ -8,14 +8,14 @@ import abis from '../abis'
 export default function WithdrawAllFromOldStaker({ className }: { className?: string }) {
   const { data } = useData()
   return <JustExecute className={className} task={{
-    verb: 'withdraw',
+    verb: 'exit',
     token: { balance: data.utilities.oldStakerBalance, symbol: 'Old Staked yPrisma', decimals: 18, allowances: [], address: '0xE3EE395C9067dD15C492Ca950B101a7d6c85b5Fc' },
     needsApproval: false,
     parameters: {
       address: env.YPRISMA_OLD_STAKER,
       abi: abis.OldStaker,
-      functionName: 'withdraw',
-      args: () => [data.utilities.oldStakerBalance]
+      functionName: 'exit',
+      args: () => [],
     }
   }} />
 }
