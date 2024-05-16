5b75ceeeb1cf9155b472e19f6d0b56f10dd43de9
Marco Guaspari Worms <marcogworms@gmail.com>
2024-05-16 09:47:34 -0300

    rename image to not confuse old img cache

--- app/components/Background.tsx
+++ app/components/Background.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
 
 export default function Background({ className }: { className?: string }) {
-  return <Image className={`absolute left-[24%] w-[76%] ${className}`} src="/prisma.svg" width={1600} height={682} alt="" />
+  return <Image className={`absolute left-[24%] w-[76%] ${className}`} src="/prisma-bg.svg" width={1600} height={682} alt="" />
 }
