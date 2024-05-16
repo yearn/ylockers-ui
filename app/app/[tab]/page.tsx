ffdf921de418b8dd79c69268c5bf9ccd8d5d4b8d
Marco Guaspari Worms <marcogworms@gmail.com>
2024-05-16 14:35:53 -0300

    fix total vault balance when not logged in

--- app/app/[tab]/page.tsx
+++ app/app/[tab]/page.tsx
@@ -170,7 +170,7 @@ export default function Home() {
                     <span className="font-semibold pb-4 pt-6 text-lg border-t-2 border-soft-blue">TOTAL DEPOSITS</span>
                     <div className="flex justify-between">
                       <span className="font-thin opacity-70	">yPRISMA Deposited</span>
-                      <span className="font-bold">{data.strategy.balance
+                      <span className="font-bold">{data.strategy.totalAssets
                         ? bmath.div(data.strategy.totalAssets, 10n**18n).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })
                         : '0.00'
                       }
