029461fb029dd0f3b450c4480642e993c8a78af9
murderteeth <89237203+murderteeth@users.noreply.github.com>
2024-05-30 16:01:40 -0400

    Use active aprs (#78)
    * Switch from projected to active aprs
    
    * Fix estimated and historic apr displays on prisma vault

--- hooks/useVault.ts
+++ hooks/useVault.ts
@@ -4,7 +4,6 @@ import useSWR from 'swr'
 export default function useVault(vault: `0x${string}`) {
   const request = `${env.YDAEMON}/1/vault/${vault}`
 
-
   const { data, isLoading, isValidating, error, mutate } = useSWR(request, async () => {
     const response = await fetch(request)
     return response.json()
