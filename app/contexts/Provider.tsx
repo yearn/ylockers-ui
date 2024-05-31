a8abd71375fed88931bdeb46d40b4ee1aa7320fa
murderteeth <murdertxxth@gmail.com>
2024-05-31 22:38:08 +0000

    Match wallet list with yearn.fi

--- app/contexts/Provider.tsx
+++ app/contexts/Provider.tsx
@@ -4,6 +4,7 @@ import {
   getDefaultConfig,
   RainbowKitProvider,
 } from '@rainbow-me/rainbowkit';
+import { injectedWallet, frameWallet, metaMaskWallet, walletConnectWallet, rainbowWallet, coinbaseWallet, safeWallet } from '@rainbow-me/rainbowkit/wallets';
 import { WagmiProvider } from 'wagmi';
 import {
   mainnet,
@@ -31,6 +32,18 @@ const config = getDefaultConfig({
   projectId: '84801a4fb569adb34f184f543b6d1762',
   // chains: [newMainnet],
   chains: [mainnet],
+  wallets: [{
+    groupName: 'Popular',
+    wallets: [
+      injectedWallet,
+      frameWallet,
+      metaMaskWallet,
+      walletConnectWallet,
+      rainbowWallet,
+      coinbaseWallet,
+      safeWallet
+    ]
+  }],
   ssr: true, // If your dApp uses server side rendering (SSR)
 });
 
