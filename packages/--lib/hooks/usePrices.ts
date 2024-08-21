3d768588a8fa61c4e6d26af1f90bf9bbc7b4c85f
murderteeth <murdertxxth@gmail.com>
2024-08-21 20:21:58 +0000

    Include locker token peg-pricing in usePrices result

--- packages/--lib/hooks/usePrices.ts
+++ packages/--lib/hooks/usePrices.ts
@@ -1,10 +1,29 @@
+import { useMemo } from 'react'
 import env from '../tools/env'
 import useSWR from 'swr'
+import { usePeg } from './usePeg'
+
+function useLockerTokenPriceBasedOnPeg(baseTokenPrice: number | undefined) {
+  const peg = usePeg()
+  const result = useMemo(() => {
+    if (baseTokenPrice === undefined) return undefined
+    return baseTokenPrice * peg
+  }, [baseTokenPrice, peg])
+  return result
+}
 
 export default function usePrices(tokens: `0x${string}`[]) {
-  const request = `${env.YDAEMON}/1/prices/some/${tokens.join(',')}?humanized=true`
+  const _tokens = useMemo(() => {
+    const result: `0x${string}`[] = [...tokens]
+    if (tokens.includes(env.LOCKER_TOKEN) && !tokens.includes(env.BASE_TOKEN)) {
+      result.push(env.BASE_TOKEN)
+    }
+    return result
+  }, [tokens])
 
-  const fallbackData = tokens.reduce((acc: { [key: `0x${string}`]: number }, token) => {
+  const request = `${env.YDAEMON}/1/prices/some/${_tokens.join(',')}?humanized=true`
+
+  const fallbackData = _tokens.reduce((acc: { [key: `0x${string}`]: number }, token) => {
     acc[token] = 0
     return acc
   }, {})
@@ -17,8 +36,14 @@ export default function usePrices(tokens: `0x${string}`[]) {
     refreshInterval: 30_000
   })
 
+  const lockerTokenPrice = useLockerTokenPriceBasedOnPeg(data[env.BASE_TOKEN])
+
+  const _data = useMemo(() => {
+    return { ...data, [env.LOCKER_TOKEN]: lockerTokenPrice ?? 0 }
+  }, [data, lockerTokenPrice])
+
   return {
-    data: data as { [key: `0x${string}`]: number },
+    data: _data as { [key: `0x${string}`]: number },
     isLoading,
     isValidating,
     error,
