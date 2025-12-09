ffa98a98599fdde3ed5cb14208442db631af5d21
0xeye <97349378+0xeye@users.noreply.github.com>
2025-12-09 10:38:05 +0000

    chore: adjust gradient

--- packages/yb/components/Background.tsx
+++ packages/yb/components/Background.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
 
 export default function Background({className}: {className?: string}) {
-	return <div className="fixed w-full h-screen bg-gradient-to-b from-[#262626] to-[#1B1B1B]" />;
+	return <div className="fixed w-full h-screen bg-gradient-to-b from-[#363636] to-[#1B1B1B]" />;
 }
