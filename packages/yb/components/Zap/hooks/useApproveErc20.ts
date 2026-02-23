08999c1c190ba3f8d9b43ca7ffd10d48a3d1c84b
Ross <ross@yearn.finance>
2026-02-23 15:09:54 -0700

    Fix: approval issues

--- packages/yb/components/Zap/hooks/useApproveErc20.ts
+++ packages/yb/components/Zap/hooks/useApproveErc20.ts
@@ -26,7 +26,7 @@ export function useApproveErc20() {
 		}
 	});
 
-	const parameters = useMemo<UseSimulateContractParameters>(
+	const maxParameters = useMemo<UseSimulateContractParameters>(
 		() => ({
 			abi: erc20Abi,
 			address: inputToken?.address ?? zeroAddress,
@@ -34,18 +34,37 @@ export function useApproveErc20() {
 			args: [ZAP, maxUint256],
 			chainId: 1,
 			query: {
-				enabled:
-					isConnected &&
-					inputToken !== undefined &&
-					!inputIsYbs &&
-					allowance.isFetched &&
-					allowance.data! === 0n
+				enabled: isConnected && inputToken !== undefined && !inputIsYbs && allowance.isFetched
 			}
 		}),
 		[isConnected, inputToken, inputIsYbs, allowance]
 	);
 
-	const simulation = useSimulateContract(parameters);
+	const resetParameters = useMemo<UseSimulateContractParameters>(
+		() => ({
+			abi: erc20Abi,
+			address: inputToken?.address ?? zeroAddress,
+			functionName: 'approve',
+			args: [ZAP, 0n],
+			chainId: 1,
+			query: {
+				enabled:
+					isConnected &&
+					inputToken !== undefined &&
+					!inputIsYbs &&
+					allowance.isFetched &&
+					(allowance.data ?? 0n) > 0n
+			}
+		}),
+		[isConnected, inputToken, inputIsYbs, allowance]
+	);
+
+	const simulationMax = useSimulateContract(maxParameters);
+	const simulationReset = useSimulateContract(resetParameters);
+	const simulation = useMemo(
+		() => (simulationMax.data?.request ? simulationMax : simulationReset),
+		[simulationMax, simulationReset]
+	);
 	const write = useWriteContract();
 	const confirmation = useWaitForTransactionReceipt({hash: write.data});
 
